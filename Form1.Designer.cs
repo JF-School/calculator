f5e0294c0abf2da59433b1effc529655cefb4f83
Joshua Figueiredo <339727042@GoTVDSB.ca>
2024-10-23 09:40:55 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace calculators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.lblSubtract = new System.Windows.Forms.Label();
+            this.lblTimes = new System.Windows.Forms.Label();
+            this.lblDivide = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 24);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(86, 83);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(12, 127);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(86, 83);
+            this.btnSubtract.TabIndex = 1;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(12, 231);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(86, 83);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "×";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(12, 337);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(86, 83);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "÷";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdd.Location = new System.Drawing.Point(116, 50);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(86, 31);
+            this.lblAdd.TabIndex = 4;
+            this.lblAdd.Text = "label1";
+            this.lblAdd.Visible = false;
+            // 
+            // lblSubtract
+            // 
+            this.lblSubtract.AutoSize = true;
+            this.lblSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtract.Location = new System.Drawing.Point(116, 153);
+            this.lblSubtract.Name = "lblSubtract";
+            this.lblSubtract.Size = new System.Drawing.Size(86, 31);
+            this.lblSubtract.TabIndex = 5;
+            this.lblSubtract.Text = "label2";
+            this.lblSubtract.Visible = false;
+            // 
+            // lblTimes
+            // 
+            this.lblTimes.AutoSize = true;
+            this.lblTimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimes.Location = new System.Drawing.Point(116, 257);
+            this.lblTimes.Name = "lblTimes";
+            this.lblTimes.Size = new System.Drawing.Size(86, 31);
+            this.lblTimes.TabIndex = 6;
+            this.lblTimes.Text = "label3";
+            this.lblTimes.Visible = false;
+            // 
+            // lblDivide
+            // 
+            this.lblDivide.AutoSize = true;
+            this.lblDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivide.Location = new System.Drawing.Point(116, 363);
+            this.lblDivide.Name = "lblDivide";
+            this.lblDivide.Size = new System.Drawing.Size(86, 31);
+            this.lblDivide.TabIndex = 7;
+            this.lblDivide.Text = "label4";
+            this.lblDivide.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 450);
+            this.Controls.Add(this.lblDivide);
+            this.Controls.Add(this.lblTimes);
+            this.Controls.Add(this.lblSubtract);
+            this.Controls.Add(this.lblAdd);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.Label lblSubtract;
+        private System.Windows.Forms.Label lblTimes;
+        private System.Windows.Forms.Label lblDivide;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            int num1, num2;
+            num1 = generator.Next(1, 1001);
+            num2 = generator.Next(1, 1001);
+            lblAdd.Visible = true;
+            lblAdd.Text = num1 + " + " + num2 + " = " + (num1 + num2);
+            btnAdd.Enabled = false;
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            int num1, num2;
+            num1 = generator.Next(1, 1001);
+            num2 = generator.Next(1, 1001);
+            lblSubtract.Visible = true;
+            lblSubtract.Text = num1 + " - " + num2 + " = " + (num1 - num2);
+            btnSubtract.Enabled = false;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            int num1, num2;
+            num1 = generator.Next(1, 101);
+            num2 = generator.Next(1, 101);
+            lblTimes.Visible = true;
+            lblTimes.Text = num1 + " × " + num2 + " = " + (num1 * num2);
+            btnMultiply.Enabled = false;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            Random generator = new Random();
+            double num1, num2;
+            num1 = generator.Next(1, 101);
+            num2 = generator.Next(1, 101);
+            lblDivide.Visible = true;
+            lblDivide.Text = num1 + " ÷ " + num2 + " = " + Math.Round((num1 / num2), 3);
+            btnDivide.Enabled = false;
+        }
+    }
+}
